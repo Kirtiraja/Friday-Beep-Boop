e98480f9919568c6937b96ba22d486e2998562ad
Kirtiraja Zakheim <kirtiraja@gmail.com>
2019-11-01 13:00:36 -0700

    non-working forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -23,12 +23,16 @@ $(document).ready(function() {
     var finalResult = userInput;
 
     var userInput = parseInt($("#userNumber").val());
-
+    var beep = 1
      var userArray = [];
      for (index = 0; index <= userInput ; index += 1) {
        userArray.push(index);
      }
-
+     userArray.forEach(function(number){
+       if (beep.includes(number[0])){
+         return "Beep!"
+       }
+     })
      console.log(userArray);
 
       // var userSplit = []
