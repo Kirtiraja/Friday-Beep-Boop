0142f8a732a9ae5deb106ca1ef71d275fa5ae976
Kirtiraja Zakheim <kirtiraja@gmail.com>
2019-11-06 22:54:54 -0800

    add commented out plans to create forEach loop to complete project

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ var userArray = [];
 var makeUserArray = function(userInput) {
   for (index = 0; index <= userInput ; index += 1) {
     userArray.push(index);
-    console.log(mappedArray)
+    console.log(mappedArray);
     var mappedArray = userArray.map(function(number){
-      if (number === 1 || number ===3){
+      if (number === 1 || number ===3){ //put this line of code into a forEach loop so each element in the new array is anaylyzed or a 3,2,or1---
         return "beep";
       }
       else {
@@ -35,7 +35,6 @@ $(document).ready(function() {
     var beepResult = makeUserArray(userInput);
     // var beepResult = makeBeep(userArray);
       $("p#result").text(beepResult);
-      console.log(userArray);
 
 
 
