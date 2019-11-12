5ba2d22905aa9042f8b131cb260ca4876f4d708b
Kirtiraja Zakheim <kirtiraja@gmail.com>
2019-11-11 21:38:52 -0800

    add fixed foreach loop with .string and .includes to complete business logic for changing numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,31 @@
 //BUSINESS LOGIC
-var userArray = [];
-var makeUserArray = function(userInput) {
+function beepSentence(userInput) {
+  if (isNaN(userInput) === true || userInput < 1) {
+    return alert ("please enter number.");
+  }
+
+  var userArray = [];
   for (index = 0; index <= userInput ; index += 1) {
     userArray.push(index);
-    console.log(mappedArray);
-    var mappedArray = userArray.map(function(number){
-      if (number === 1 || number ===3){ //put this line of code into a forEach loop so each element in the new array is anaylyzed or a 3,2,or1---
-        return "beep";
-      }
-      else {
-        return number;
-      }
-    })
-    var joinedArray = mappedArray.join("");
-  }
-}
 
-function makeBeep(userArray){
-  var numbers = [1, 2, 3];
-  if (numbers.includes(number)) {
-    return number + 1;
   }
-  else {
-    return number ;
-  }
+  var makeBeeps = [];
+  userArray.forEach(function(number){
+    if (number.toString().includes("3") === true) {
+      makeBeeps.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    }
+    else if  (number.toString().includes("2") === true){
+      makeBeeps.push("Boop!");
+    }
+    else if (number.toString().includes("1") === true) {
+      makeBeeps.push("Beep!");
+    }
+    else {
+      makeBeeps.push(number);
+    }
+  });
+
+  return makeBeeps;
 }
 
 
@@ -33,8 +35,7 @@ $(document).ready(function() {
   $("form#userForm").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($("#userNumber").val());
-    var beepResult = makeUserArray(userInput);
-    // var beepResult = makeBeep(userArray);
+    var beepResult = beepSentence(userInput);
       $("p#result").text(beepResult);
 
 
